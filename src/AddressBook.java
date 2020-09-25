698930f2792726130b55939f58b4ac5951982af1
phucla2201 <phucla2201@gmail.com>
2020-09-24 23:25:04 -0400

    Print of the information about buddy to the main method

---
+++ src/AddressBook.java
@@ -0,0 +1,39 @@
+import java.util.LinkedList;
+
+public class AddressBook {
+    public LinkedList<BuddyInfo> addressbook;
+
+    public AddressBook() {
+        addressbook = new LinkedList<BuddyInfo>();
+    }
+
+    /**
+     * This method add a friend's informataion into the address book
+     * @param buddy is the information of the friend
+     */
+    private void addBuddy(BuddyInfo buddy) {
+        addressbook.add(buddy);
+    }
+
+
+    /**
+     * This method remove the first friend's information in the address book
+     */
+    private BuddyInfo removeBuddy(){
+       return addressbook.pop();
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo rob = new BuddyInfo("Robell", "Somewhere in greensboro", "613 - the rest saved in my phone");
+        BuddyInfo pou = new BuddyInfo("Pouya", "5 mins away from me", " 613 - the rest saved in my phone");
+        BuddyInfo temp, temp2;
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(rob);
+        addressBook.addBuddy(pou);
+        temp = addressBook.removeBuddy();
+        addressBook.addBuddy(rob);
+        temp2 = addressBook.removeBuddy();
+        System.out.println("Name: "+ temp.getName() + " Address: " + temp.getAddress() + " Phone#: " + temp.getPhonenum());
+
+    }
+}
