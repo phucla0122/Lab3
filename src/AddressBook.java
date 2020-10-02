2174a2c529ce34f75f7ca8bbb4210eb7c5ede5f7
phucla2201 <phucla2201@gmail.com>
2020-10-01 23:35:54 -0400

    Add one new friend info and display that info

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -35,7 +35,7 @@ public class AddressBook {
         addressBook.addBuddy(rob);
         temp2 = addressBook.removeBuddy();
         temp3 = addressBook.removeBuddy();
-        tem4 = addressBook.removeBuddy();
+        temp4 = addressBook.removeBuddy();
         System.out.println("Name: "+ temp.getName() + " Address: " + temp.getAddress() + " Phone#: " + temp.getPhonenum());
         System.out.println("Name: "+ temp2.getName() + " Address: " + temp2.getAddress() + " Phone#: " + temp2.getPhonenum());
         System.out.println("Name: "+ temp4.getName() + " Address: " + temp4.getAddress() + " Phone#: " + temp4.getPhonenum());
