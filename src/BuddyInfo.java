21033f6422fb2c9abc90626f0eb40c44d10a0139
phucla2201 <phucla2201@gmail.com>
2020-10-01 22:26:31 -0400

    Print of the 2nd information about buddy to the main method

---
+++ src/BuddyInfo.java
@@ -0,0 +1,23 @@
+public class BuddyInfo {
+
+    private String name, address, phonenum;
+
+    public BuddyInfo(String name, String address, String phonenum) {
+        this.name = name;
+        this.address = address;
+        this.phonenum = phonenum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhonenum() {
+        return phonenum;
+    }
+}
+
